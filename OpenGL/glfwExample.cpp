817eb4d2b0e3d38b20d5445abe3f83261e7fdef4
lee03840 <lee03840@d.umn.edu>
2026-04-12 15:09:55 -0500

    Spheres and ListOfTriangles handling

--- OpenGL/glfwExample.cpp
+++ OpenGL/glfwExample.cpp
@@ -1,6 +1,9 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <fstream>
+#include <sstream>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -12,10 +15,87 @@
 
 #include "GLSL.h"
 
+
+
 int CheckGLErrors(const char *s)
 {
     int errCount = 0;
     return errCount;
+}
+
+void generateSphere(glm::vec3 center, float radius, int slices, int stacks, std::vector<float>& vbo)
+{
+  for (int i = 0; i <= stacks; i++) {
+    float theta = i * std::numbers::pi / stacks;
+
+    for (int j = 0; j <= slices; j++) {
+      float phi = j * 2.0f * std::numbers::pi / slices;
+
+      // Vertices
+      float x = radius * sin(theta) * cos(phi);
+      float y = radius * cos(theta);
+      float z = radius * sin(theta) * sin(phi);
+
+      // Normals
+      float nx = x / radius;
+      float ny = y / radius;
+      float nz = z / radius;
+
+      vbo.push_back(center.x + x);
+      vbo.push_back(center.y + y);
+      vbo.push_back(center.z + z);
+      vbo.push_back(nx);
+      vbo.push_back(ny);
+      vbo.push_back(nz);
+    }
+  }
+}
+
+void generateSphereIndices(int stacks, int slices, std::vector<unsigned int>& indices)
+{
+  for (int i = 0; i < stacks; i++) {
+    for (int j = 0; j < slices; j++) {
+      int row1 = i * (slices + 1);
+      int row2 = (i + 1) * (slices + 1);
+
+      // Triangle 1
+      indices.push_back(row1 + j);
+      indices.push_back(row1 + j + 1);
+      indices.push_back(row2 + j + 1);
+
+      // Triangle 2
+      indices.push_back(row1 + j);
+      indices.push_back(row2 + j + 1);
+      indices.push_back(row2 + j);
+    }
+  }
+}
+
+GLuint loadSphere(std::vector<float> vbo, std::vector<unsigned int> indices)
+{
+  GLuint sphereVBO;
+  glGenBuffers(1, &sphereVBO);
+  glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+  glBufferData(GL_ARRAY_BUFFER, vbo.size() * sizeof(float), vbo.data(), GL_STATIC_DRAW);
+
+  // EBO
+  GLuint sphereEBO;
+  glGenBuffers(1, &sphereEBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+  // VAO
+  GLuint sphereVAO;
+  glGenVertexArrays(1, &sphereVAO);
+  glBindVertexArray(sphereVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void *)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void *)(3 * sizeof(GLfloat)));
+  glBindVertexArray(0);
+  return sphereVAO;
 }
 
 int main(void)
@@ -94,7 +174,87 @@ int main(void)
   //Load Scene file
   //Get Shapes into scene
 
-  //Load triangle
+  // List of triangles
+  std::ifstream file("../trilist.dat");
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file" << std::endl;
+  }
+
+  std::vector<glm::vec3> positions;
+  std::string tokenX, tokenY, tokenZ;
+
+  while(getline(file, tokenX, ',') &&
+        getline(file, tokenY, ',') &&
+        getline(file, tokenZ, ',')) {
+    try {
+      float x = std::stof(tokenX);
+      float y = std::stof(tokenY);
+      float z = std::stof(tokenZ);
+
+      positions.push_back(glm::vec3(x,y,z));
+
+    } catch (const std::invalid_argument& e) {
+      std::cerr << "Invalid token(s): " << tokenX << ' ' << tokenY << ' ' << tokenZ << std::endl;
+    }
+  }
+  file.close();
+  std::cout << "Vertices read: " << positions.size() / 6 << std::endl;
+
+  // Calculate Normals for each vertex in list
+  std::vector<glm::vec3> smoothNormals(positions.size(), glm::vec3(0.0f));
+  for (int i = 0; i + 2 < positions.size(); i += 3) {
+    glm::vec3 v0 = positions[i+0];
+    glm::vec3 v1 = positions[i+1];
+    glm::vec3 v2 = positions[i+2];
+
+    // Face normal
+    glm::vec3 edge1 = v1 - v0;
+    glm::vec3 edge2 = v2 - v0;
+    glm::vec3 faceNormal = glm::normalize(glm::cross(edge1, edge2));
+
+    smoothNormals[i+0] += faceNormal;
+    smoothNormals[i+1] += faceNormal;
+    smoothNormals[i+2] += faceNormal;
+  }
+
+  //Build VBO
+  std::vector<float> vbo_listOfTriangles;
+  for (int i = 0; i < positions.size(); i++) {
+    glm::vec3 v = positions[i];
+    glm::vec3 n = glm::normalize(smoothNormals[i]);
+
+    vbo_listOfTriangles.push_back(v.x);
+    vbo_listOfTriangles.push_back(v.y);
+    vbo_listOfTriangles.push_back(v.z);
+    vbo_listOfTriangles.push_back(n.x);
+    vbo_listOfTriangles.push_back(n.y);
+    vbo_listOfTriangles.push_back(n.z);
+  }
+
+  GLuint triVBO, triVAO;
+
+  glGenBuffers(1, &triVBO);
+  glBindBuffer(GL_ARRAY_BUFFER, triVBO);
+  glBufferData(GL_ARRAY_BUFFER, vbo_listOfTriangles.size() * sizeof(float), vbo_listOfTriangles.data(), GL_STATIC_DRAW);
+
+  glGenVertexArrays(1, &triVAO);
+  glBindVertexArray(triVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, triVBO);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glBindVertexArray(0);
+
+  // Sphere
+  std::vector<float> vboSphere;
+  std::vector<unsigned int> indices;
+
+  generateSphere(glm::vec3(0,0,0), 1.0f, 20, 20, vboSphere);
+  generateSphereIndices(20, 20, indices);
+  GLuint sphereVAO = loadSphere(vboSphere, indices);
+
+  //Triangle
   GLuint m_triangleVBO[1], m_VAO;
 
   glGenBuffers(1, m_triangleVBO);
@@ -102,9 +262,9 @@ int main(void)
 
   std::vector<float> host_VertexBuffer{
     // Position           //Normal
-    -3.0f, -3.0f, 0.0f,   0.0f, 0.0f, 1.0f,
-    3.0f, -3.5f, 0.0f,   0.0f, 0.0f, 1.0f,
-    0.0f, 3.0f, 0.0f,    0.0f, 0.0f, 1.0f,};
+    -3.0f, 3.0f, 0.0f,   0.0f, 0.0f, 1.0f,
+    3.0f, 3.5f, 0.0f,   0.0f, 0.0f, 1.0f,
+    0.0f, 6.0f, 0.0f,    0.0f, 0.0f, 1.0f,};
 
   int numBytes = host_VertexBuffer.size() * sizeof(float);
 
@@ -133,7 +293,7 @@ int main(void)
   // Create a shader using my GLSLObject class
   sivelab::GLSLObject shader;
   shader.addShader( "vertexShader_PrepForPerFragment.glsl", sivelab::GLSLObject::VERTEX_SHADER );
-  shader.addShader( "fragmentShader_Normal.glsl", sivelab::GLSLObject::FRAGMENT_SHADER );
+  shader.addShader( "fragmentShader_BlinnPhong.glsl", sivelab::GLSLObject::FRAGMENT_SHADER );
   shader.createProgram();
 
   GLuint projMatrixID, viewMatrixID, modelMatrixID, normalMatrixID;
@@ -142,6 +302,8 @@ int main(void)
   modelMatrixID = shader.createUniform("modelMatrix");
   normalMatrixID = shader.createUniform("normalMatrix");
 
+
+  // Blinn-Phong
   GLuint lightPosID = shader.createUniform("lightPosWorld");
   GLuint intensityID = shader.createUniform("intensity");
   GLuint laID = shader.createUniform("la");
@@ -208,26 +370,49 @@ int main(void)
     glUniformMatrix4fv(modelMatrixID,  1, GL_FALSE, glm::value_ptr(modelTransform));
     glUniformMatrix4fv(normalMatrixID, 1, GL_FALSE, glm::value_ptr(normalMatrix));
 
-    // Draw
-    glBindVertexArray(m_VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // Draw List of Triangles
+    glBindVertexArray(triVAO);
+    glDrawArrays(GL_TRIANGLES, 0, vbo_listOfTriangles.size() / 6);
     glBindVertexArray(0);
 
+    // Draw Triangle
+    glm::mat4 triangleModel = glm::mat4(1.0f);
+    triangleModel = glm::rotate(triangleModel, rot, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 triangleNormal = glm::transpose(glm::inverse(M_view * triangleModel));
+    glUniformMatrix4fv(modelMatrixID, 1, GL_FALSE, glm::value_ptr(triangleModel));
+    glUniformMatrix4fv(normalMatrixID, 1, GL_FALSE, glm::value_ptr(triangleNormal));
+    glBindVertexArray(m_VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // Draw Sphere
+    glm::mat4 sphereModel = glm::translate(glm::mat4(1.0f), glm::vec3(4.0f, 0.0f, 0.0f));
+    glm::mat4 sphereNormal = glm::mat4(glm::transpose(glm::inverse(M_view * glm::mat4(sphereModel))));
+    glUniformMatrix4fv(modelMatrixID, 1, GL_FALSE, glm::value_ptr(sphereModel));
+    glUniformMatrix4fv(normalMatrixID, 1, GL_FALSE, glm::value_ptr(sphereNormal));
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+
+    glBindVertexArray(0);
     shader.deactivate();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
 
     // Camera movement
-    float moveRatePerFrame = 0.05f;
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    float moveRatePerFrame = 0.005f;
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
       m_pos = m_pos + -m_W * moveRatePerFrame;
     else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
       m_pos = m_pos - m_U * moveRatePerFrame;
-    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    else if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
       m_pos = m_pos + m_W * moveRatePerFrame;
     else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
       m_pos = m_pos + m_U * moveRatePerFrame;
+    else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+      m_pos = m_pos + m_V * moveRatePerFrame;
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+      m_pos = m_pos - m_V * moveRatePerFrame;
+
 
     if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
       std::cout << "fps: " << 1.0 / timeDiff << std::endl;
